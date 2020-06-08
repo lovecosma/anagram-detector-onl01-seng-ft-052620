2096278177046af757d35d56c406a9f8908bcd51
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-08 18:05:51 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+attr_accessor :word
+
+def initialize(word)
+  @word = word
+end
+
+def match(array)
+  final_array = []
+array.each do |word|
+  split_array = word.chars
+  self_array = self.word.chars
+  if self_array.all? {|char| split_array.include?(char)}
+    final_array << word if word.length == self.word.length
+  end
+  end
+  final_array
+end
+
+
+end
